e1a029e5713cdfc4541fe7b0d5e9ebc57670d5c7
DmitriyKuzmin991 <121133178+DmitriyKuzmin991@users.noreply.github.com>
2023-01-26 23:49:07 +0300

    Update Program.cs

--- homeworkSeminarFour/homeWork2_ОпрЧисло/Program.cs
+++ homeworkSeminarFour/homeWork2_ОпрЧисло/Program.cs
@@ -1,21 +1,17 @@
 ﻿// Определить, присутствует ли в заданном массиве, некоторое число
 
-
 int[] myArray(int range, int maxValue)
 {
   int [] theArray = new int [range];
   Random rand = new Random();
-
-  for (int i = 0; i < theArray.Length; i++)
-  {
-    theArray[i] = rand.Next(maxValue); 
-  }
-  
+    for (int i = 0; i < theArray.Length; i++)
+    {
+     theArray[i] = rand.Next(maxValue); 
+    }
   return theArray;
 }
 
 int[] theArray = myArray(10, 11);
-//System.Console.WriteLine(string.Join(";", theArray));
 System.Console.Write("Введите число для поиска в массиве от 0 до 10:\t");
 int findCount = int.Parse(Console.ReadLine());
 int result = 0;
@@ -26,7 +22,6 @@ for (int i = 0; i < theArray.Length; i++)
     result = 1;
     break;
   }
-  
 }
 
 if (result == 1)
